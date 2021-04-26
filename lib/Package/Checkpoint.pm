40b9edca42065dd3c3b866aaa22f2fd3cb2ec988
Graham Ollis <plicease@cpan.org>
2021-04-26 15:45:41 -0600

    Initial structure

---
+++ lib/Package/Checkpoint.pm
@@ -0,0 +1,12 @@
+package Package::Checkpoint;
+
+use strict;
+use warnings;
+use 5.008004;
+
+# ABSTRACT: Checkpoint the scalar, array and hash values in a package for later restoration
+# VERSION
+
+1;
+
+
